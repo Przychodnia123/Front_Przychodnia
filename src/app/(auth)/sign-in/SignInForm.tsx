9a296d16f4e28cef01b4f0be6b1938cb78febfa4
Magda <wozna.magda@gmail.com>
2024-11-08 13:30:14 +0100

    Adjust sign in form

--- src/app/(auth)/sign-in/SignInForm.tsx
+++ src/app/(auth)/sign-in/SignInForm.tsx
@@ -6,6 +6,7 @@ import { z } from "zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { Label } from "@/src/utilities/Label";
 
 type SignInFormValues = z.infer<typeof SignInValidationSchema>;
 
@@ -17,27 +18,32 @@ export const SignInForm = () => {
   } = useForm<SignInFormValues>({ resolver: zodResolver(SignInValidationSchema) });
 
   const onSubmit: SubmitHandler<SignInFormValues> = (values) => {
+    console.log('values', values)
     // TODO: Handle form submission, e.g., send data to an API
   }
 
   return (
-        <form onSubmit={handleSubmit(onSubmit)} className="py-10 space-y-5 w-full h-full tablet:h-auto flex flex-col">    
-          <Input
-            type="email"
-            label="Adres e-mail"
-            placeholder="example@example.com"
-            id="email"
-            {...register('email')}
-            error={errors.email}
-          />       
-          <Input
-            type="password"
-            label="Hasło"
-            placeholder="********"
-            id="password"
-            {...register('password')}
-            error={errors.password}
-          /> 
+        <form noValidate onSubmit={handleSubmit(onSubmit)} className="py-10 space-y-5 w-full h-full tablet:h-auto flex flex-col">    
+          <div>
+            <Label>Adres e-mail</Label>
+            <Input
+              type="email"
+              placeholder="example@example.com"
+              id="email"
+              {...register('email')}
+              error={errors.email}
+            />      
+          </div> 
+          <div>
+            <Label>Hasło</Label>
+            <Input
+              type="password"
+              placeholder="********"
+              id="password"
+              {...register('password')}
+              error={errors.password}
+            />  
+          </div>
            <div className="flex justify-between">
             <label><input required type="checkbox" /><span className="ml-2">Zapamiętaj hasło</span></label>
             <Link className="text-light-blue" href="/">Nie pamiętasz hasła?</Link>
