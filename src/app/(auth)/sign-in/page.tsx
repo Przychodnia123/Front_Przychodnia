0ee8a9a311ceeda4ec3eb86033411260419b67f8
javac200 <javac2.0.0@gmail.com>
2025-01-19 17:45:24 +0100

    pr-69 - fix navigation to home page from sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -7,7 +7,9 @@ import Link from 'next/link'
 export default function SignIn() {
   return (
     <div className='flex h-auto w-full flex-col items-center gap-y-4 rounded-3xl bg-white px-5 py-10 shadow-md tablet:h-auto tablet:w-3/4 tablet:px-16 laptop:w-1/2'>
-      <Image src='/nav-assets/logo.png' alt='Logo' width={208} height={71} />
+      <Link href={routes.home}>
+        <Image src='/nav-assets/logo.png' alt='Logo' width={208} height={71} />
+      </Link>
       <Title size='text-5xl'>Zaloguj się</Title>
       <SignInForm />
       <p>
