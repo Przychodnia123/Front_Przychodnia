2a3b071195c6b09b070da717965aa47510d16cc2
Magda <wozna.magda@gmail.com>
2025-03-03 10:03:14 +0100

    fix(imports): use path aliases

--- src/services/getUser.tsx
+++ src/services/getUser.tsx
@@ -1,5 +1,5 @@
-import { User } from '../types'
-import { apiClient } from './apiClient'
+import { User } from '@customtypes/index'
+import { apiClient } from '@services/apiClient'
 
 export const getUser = (token: string) =>
   apiClient<User | null>('/user_info', { token })
