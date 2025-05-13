e28838c8740fc075e10b46d3eef0be3f6387ffa0
Magda <wozna.magda@gmail.com>
2025-05-13 17:17:02 +0200

    test(sign-up): add tests for sign up page

---
+++ __tests__/signUp.test.tsx
@@ -0,0 +1,34 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import SignUp from '@/app/(auth)/sign-up/page'
+
+describe('SignUp', () => {
+  beforeEach(() => {
+    render(<SignUp />)
+  })
+
+  it('should render sign up form with appropriate fields', () => {
+    const form = screen.getByRole('form')
+    expect(form).toBeInTheDocument()
+
+    const expectedFors = ['username', 'email', 'password', 'password2', 'terms']
+
+    expectedFors.forEach((forAttr) => {
+      const label = screen.getByLabelText((_, element) => {
+        return (
+          element?.tagName.toLowerCase() === 'input' && element.id === forAttr
+        )
+      })
+
+      expect(label).toBeInTheDocument()
+    })
+  })
+
+  it('should render link to sign up page', () => {
+    const hasAccountText = screen.getByText(/Masz już konto?/i)
+    expect(hasAccountText).toBeInTheDocument()
+
+    const link = screen.getByRole('link', { name: 'Zaloguj się' })
+    expect(link).toHaveAttribute('href', '/logowanie')
+  })
+})
